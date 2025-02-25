a731163c011f62c944e7146feac89ed54b4ba7c1
MarcellGranat <granatcellmar98@gmail.com>
2025-02-25 11:29:45 +0100

    Enhance scraping functionality with improved error handling, logging, and cookie management

--- main.py
+++ main.py
@@ -2,51 +2,92 @@ import parallel_execution
 import start_arcanum
 import asyncio
 import download
-import time
 import os
+from manage_cookie import init_cookies, read_cookie
+from loguru import logger
+from process_monitor import monitor_process, hash_folder
+import time
 
-async def download_archive(config, item):
-    """Download the archive."""
-    archive_name, archive_link = item
-    print(f"Downloading {archive_name} from {archive_link}...")
-    await start_arcanum.download_archive(config, archive_name, archive_link)
-    return archive_name
+logger.add("logs")
+
+waiting_for_limit = False
 
 async def scrape_page_along_tree(username, archive: tuple[str, str]):
     folder = "data/" + download.tidy_filename(archive[0])
         
     if not os.path.exists(folder):
-        os.mkdir(folder)
+        os.makedirs(folder)
 
     # ! txt signals that the whole archive is downloaded
     if os.path.exists(f"{folder}/archivename.txt") and len(os.listdir(folder)) > 1: 
-        print(f"{folder} already exists")
+        logger.info(f"{archive[0]} already finished")
         return
     
-    cookie = start_arcanum.read_cookies(username)
+    cookie = await read_cookie(username)
 
     async with start_arcanum.arcanum_page(cookie, headless=False) as page:
-        await page.goto(archive[1])
-        time.sleep(2)
-        await download.download_along_tree(page, folder=folder, username=username)
+        logger.info(f"Going to {archive[1]}")
+        while True:
+            try:
+                await page.goto(archive[1], timeout=30000)
+            except Exception as e:
+                logger.error(f"Failed to navigate to {archive[1]}: {e}")
+            await asyncio.sleep(2)
+            try:
+                await download.download_along_tree(page, folder=folder, username=username)
+            except FileNotFoundError:
+                logger.warning(f"Failed to download {archive[0]}. Going to this page again.")
+                continue
+            except Exception as e:
+                logger.error(f"Failed to download {archive[0]}: {e}")
+            logger.success(f"{archive[0]} finished")
+            # ! write a single txt file in the folder > signal that the whole archive is downloaded
+            with open(f"{folder}/archivename.txt", "w") as f:
+                f.write(archive[0])
+            break
 
-        # ! write a single txt file in the folder > signal that the whole archive is downloaded
-        with open(f"{folder}/archivename.txt", "w") as f:
-            f.write(archive[0])
+def is_working():
+    global waiting_for_limit
+    if waiting_for_limit:
+        time.sleep(3600 * 4)
+        logger.info("Waiting for download limit to reset")
+        waiting_for_limit = False
+    return hash_folder(folder_path="data")
 
 
+@monitor_process(is_working, timeout=600)
 async def parallel_scrape_page_along_tree(tuples_list):
     usernames = []
     if not os.path.exists("data"):
         os.mkdir("data")
 
-    for user in start_arcanum.generate_usernames():
-        # if await download.get_downloads_last_24h(user) < 4900:
+    cookies = await init_cookies(check=True, force=False)
+
+    if not cookies:
+        logger.error("No cookies found")
+        raise Exception("No cookies found")
+
+    for user in cookies:
+        if await download.get_downloads_last_24h(user) > 4700:
+            logger.info(f"User {user} has reached the download limit")
+            continue
+        if len(usernames) >= 2:
+            break # Should use maximum 2 users at a time
+
         usernames.append(user)
+
+    if not usernames:
+        logger.error("No users with download limit available")
+        asyncio.sleep(3600 * 10)
+        # TODO send notification
     
     await parallel_execution.parallel_exec(func=scrape_page_along_tree, configs=usernames, items=tuples_list)
+    logger.success("All archives downloaded")
 
 if __name__ == "__main__":
     import archive_links
-    urls = list(archive_links.generate_archive_links("https://adt.arcanum.com/hu/collection/PestiHirlap/?decade=1870#collection-contents"))
+    urls30s = list(archive_links.generate_archive_links("https://adt.arcanum.com/hu/collection/PestiHirlap/?decade=1930#collection-contents"))
+    urls20s = list(archive_links.generate_archive_links("https://adt.arcanum.com/hu/collection/PestiHirlap/?decade=1920#collection-contents"))
+    urls10s = list(archive_links.generate_archive_links("https://adt.arcanum.com/hu/collection/PestiHirlap/?decade=1910#collection-contents"))
+    urls = urls30s + urls20s + urls10s
     asyncio.run(parallel_scrape_page_along_tree(tuples_list=urls))
