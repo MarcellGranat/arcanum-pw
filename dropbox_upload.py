8e0abd2385ff9914e5f7e4df9057ecb66749d053
MarcellGranat <granatcellmar98@gmail.com>
2025-03-06 12:09:20 +0100

    improve logging

--- dropbox_upload.py
+++ dropbox_upload.py
@@ -17,20 +17,24 @@ class DropboxUpload:
             self.dbx.files_upload(f.read(), file_to.replace("data", "arcanum_pw"))
 
     def upload_folder(self, folder_from, folder_to):
+        skipped_files = 0
+
         for root, dirs, files in os.walk(folder_from):
             for filename in files:
                 local_path = os.path.join(root, filename)
                 relative_path = os.path.relpath(local_path, folder_from)
                 dropbox_path = os.path.join(folder_to, relative_path).replace("\\", "/")
-                print(f"Uploading {local_path} to {dropbox_path}")
 
                 try:
                     self.dbx.files_get_metadata(dropbox_path)
-                    print(f"File {dropbox_path} already exists. Skipping upload.")
+                    skipped_files += 1
                 except dropbox.exceptions.ApiError as err:
                     if err.error.is_path() and err.error.get_path().is_not_found():
                         with open(local_path, 'rb') as f:
                             self.dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+                            if skipped_files > 0:
+                                print(f"Skipped {skipped_files} files")
+                                skipped_files = 0
                             print(f"Uploaded {dropbox_path}")
                     else:
                         print(f"Failed to check existence of {dropbox_path}: {err}")
